9ef735146bac3e5b06d1ad8bbe06f220157b968c
Jhon Jairo Gelvez Gomez <jhonjairogelvezgomez@gmail.com>
2026-05-19 14:27:00 -0500

    fix(transeferencia): cambiar la ip (#)

--- trasnferencia/src/constants.js
+++ trasnferencia/src/constants.js
@@ -2,5 +2,5 @@
 // CONSTANTES DE API
 // ============================================
 
-export const apiUrl = "http://10.5.225.175:3044/users";
-export const apiTasks = "http://10.5.225.175:3044/tasks";
+export const apiUrl = "http://10.5.225.105:3044/users";
+export const apiTasks = "http://10.5.225.105:3044/tasks";
